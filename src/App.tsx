8c4aeb7803d92c1a5c3d7748d265a11fc14440e4
daigo <diego.dev101101@gmail.com>
2026-02-24 14:57:56 -0500

    header-home

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ function App() {
 
 
   return (
-    <div className="App" >
+    <div className="App relative" >
+      <div className='galaxy'>
+        <div className='stars1'></div>
+        <div className='stars2'></div>
+        <div className='stars3'></div>
+      </div>
       <Header />
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,39 @@
 
 const Header = () => {
+  const buttos = [
+    "Home",
+    "Contact",
+    "Proyects",
+    "GitHub",
+    "Blog"
+  ]
+  const name = "Diego Amachi"
   return (
-    <header className=" h-18 flex w-full position-sticky top-0 z-10  items-center gap-2 justify-around">
-      <div className='title'>
-       <a className='start items-center justify-center '>Diego Amachi</a>
+    <header className="  flex w-full h-screen items-center gap-2 justify-center ">
+      <div className='title w-full h-full flex titlecont'>
+        <h1 className=" text-9xl title text-[#0466c8]"> {name.toLocaleUpperCase()}</h1>
+        <div className=" text-6xl text-nowrap devstyle text-[#ffea00]">
+          Developer Engineer  
+        </div>
       </div>
-      <nav className=" flex justify-around">
-        <a className=" rounded ">Home</a>
-        <a>About me</a>
-        <a>Contact</a>
-        <a>Projects</a>
-      </nav>
+      <div className=" fixed  bottom-0 w-full">
+        <nav className="  fontstart gap-5 flex justify-around content-center  w-full top-0 z-10 p-4   px-3 py-2">
+          <ul className=" bg-black p-1 box-border list-none flex gap-1 ">
+            { buttos.map((nav, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`
+                      py-2 px-4  text-center text-2xl
+                      transition-all duration-150 text-gray-300 bg-[#0466c8] cursor-pointer box-border border-4 border-t-[#5fadfc] border-l-[#5fadfc] border-r-[#003367] border-b-[#003367] min-w-[150px] active:border-t-[#003367] active:border-l-[#003367] active:border-r-[#5fadfc] active:border-b-[#5fadfc] hover:text-white active:text-white select-none w-1/5
+                    `}
+                  >
+                    {nav}
+                  </li>
+                )}) }
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
